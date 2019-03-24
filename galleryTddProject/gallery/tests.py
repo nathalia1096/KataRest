6ae6b8e798ee8f5dcef5da707ebbf00f256c08c0
NATHALIA ALVAREZ <nathalia.alvarez@globant.com>
2019-03-24 01:22:19 -0500

    Paso 2-Cantidad de elementos de la lista de portafolios (ROJO)

--- galleryTddProject/gallery/tests.py
+++ galleryTddProject/gallery/tests.py
@@ -11,3 +11,15 @@ class GalleryTestCase(TestCase):
         url = '/gallery/getPortfolios/'
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, 200)
+
+    def test_count_portfolios_list(self):
+        user_model = User.objects.create_user(username='test', password='kd8wke-DE34', first_name='test',
+                                              last_name='test',
+                                              email='test@test.com')
+        Portfolio.objects.create(user=user_model)
+        Portfolio.objects.create(user=user_model)
+
+        response = self.client.get('/gallery/getPortfolios/')
+        current_data = json.loads(response.content)
+
+        self.assertEqual(len(current_data), 2)
