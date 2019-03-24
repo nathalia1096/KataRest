5364eafa2a2037129f75f1d1fc5c3fc78c21511d
NATHALIA ALVAREZ <nathalia.alvarez@globant.com>
2019-03-24 01:04:27 -0500

    change settings

--- galleryTddProject/galleryTddProject/settings.py
+++ galleryTddProject/galleryTddProject/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dbgallerytdd',
-        'USER': 'usdbgallerytdd',
+        'USER': 'usdbgallery',
         'PASSWORD': 'Abcd123#',
         'HOST': 'localhost',
         'PORT': ''
